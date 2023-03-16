b5df956440790394924aaa35238dc892fa26e2e6
John <jschriem@gmail.com>
2023-03-16 09:58:41 -0700

    fix incorrect path for file reading

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # core-website
+### Required blog structure for md -> json parser
 
-## 
-
-Public content for natureblocks.com
-
-Hello
-
-This is Matt's edit.
+title: # Title Name
+description: ## Short catchy description to be placed on cards
+author: ### Author Fullname
+date: #### Mar 16, 2023
+tags: ##### sustainability,projects,carbon accounting,cyrptocurrency
+content: everything after tags
--- publish.py
+++ publish.py
@@ -48,48 +48,24 @@ def main():
 
     # set the path to the folder containing the markdown posts
     folder_path = pathlib.Path("blog")
+
     posts = list()
 
     # get a list of all the markdown files in the folder
     markdown_files = [f for f in folder_path.iterdir() if f.name.endswith(".md")]
 
-    # loop through the markdown files
     for markdown_file in markdown_files:
         # open the markdown file and read its contents
-        with open(os.path.join(folder_path, markdown_file), "r") as f:
+        with open(os.path.join('', markdown_file), "r") as f:
             markdown_content = f.read()
 
-        # print the HTML content
         post = convert_md_to_dict(markdown_content)
         posts.append(post)
 
     response = requests.post(url, headers=headers, data=json.dumps(posts))
     results = response
 
-    print(results.json)
-
-    query = {
-        "query": {
-            "terms": {
-                "_id": [
-                    "what-is-the-carbon-footprint-of-digital-currency-and-cryptocurrency?",
-                    "what-is-tether-(usdt)?",
-                ]
-            }
-        }
-    }
-
-    getpost = requests.get(
-        url,
-        headers=headers,
-        # data=json.dumps(query)
-        json=[
-            "what-is-the-carbon-footprint-of-digital-currency-and-cryptocurrency?",
-            "what-is-tether-(usdt)?",
-        ],
-    )
-
-    print(len(getpost.json()))
+    print(results.json())
 
 
 if __name__ == "__main__":
